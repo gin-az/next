d8597689e50a894af1f69dfd31acf1b7eb0e64f7
AGindullin <GA>
2023-05-17 15:09:06 +0300

    Получение данных (Fetching Data). Обработка ошибок ассинхронных запросов

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -2,13 +2,32 @@ import Heading from "../../components/Heading";
 import React from "react";
 import Head from "next/head";
 
-const Index = () => {
+import s from "../../styles/Card.module.scss";
+
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  if (!data) {
+    return { notFound: true };
+  } //  случае ошибки вернется странится 404
+  return { props: { contacts: data } };
+};
+
+const Index = ({ contacts }) => {
   return (
     <>
       <Head>
         <title>Contacts</title>
       </Head>
-      <Heading text="CONTACTS!" />
+      <Heading text="CONTACTS" />
+      <div>
+        {contacts?.map(({ id, name, email }) => (
+          <div key={id} className={s.card}>
+            {name} | email: {email}
+          </div>
+        ))}
+      </div>
     </>
   );
 };
